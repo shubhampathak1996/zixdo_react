fcf4763a150ce0ab64466213fd084182eaf53a78
shubham pathak <shubhampathak1996@gmail.com>
2023-07-10 15:58:50 +0530

    alpha

--- src/components/our-centers/AlphabeticalSearch.js
+++ src/components/our-centers/AlphabeticalSearch.js
@@ -4,67 +4,67 @@ function AlphabeticalSearch({ setText, text, centers }) {
   console.log('centers', centers);
   return (
     <>
-      <section className='sec-50 alphabetical-search'>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-md-12'>
-              <div className='alpha-box'>
+      <section className="sec-50 alphabetical-search">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="alpha-box">
                 <div onClick={() => setText('A')}>A</div>
                 <div onClick={() => setText('B')}>B</div>
-                <div>C</div>
-                <div>D</div>
-                <div>E</div>
-                <div>F</div>
-                <div>G</div>
-                <div>H</div>
-                <div>I</div>
-                <div>J</div>
-                <div>K</div>
-                <div>L</div>
-                <div>M</div>
-                <div>N</div>
-                <div>O</div>
-                <div>P</div>
-                <div>Q</div>
-                <div>R</div>
-                <div>S</div>
-                <div>T</div>
-                <div>U</div>
-                <div>V</div>
-                <div>W</div>
-                <div>X</div>
-                <div>Y</div>
-                <div>Z</div>
+                <div onClick={() => setText('C')}>C</div>
+                <div onClick={() => setText('D')}>D</div>
+                <div onClick={() => setText('E')}>E</div>
+                <div onClick={() => setText('F')}>F</div>
+                <div onClick={() => setText('G')}>G</div>
+                <div onClick={() => setText('H')}>H</div>
+                <div onClick={() => setText('I')}>I</div>
+                <div onClick={() => setText('J')}>J</div>
+                <div onClick={() => setText('K')}>K</div>
+                <div onClick={() => setText('L')}>L</div>
+                <div onClick={() => setText('M')}>M</div>
+                <div onClick={() => setText('N')}>N</div>
+                <div onClick={() => setText('O')}>O</div>
+                <div onClick={() => setText('P')}>P</div>
+                <div onClick={() => setText('Q')}>Q</div>
+                <div onClick={() => setText('R')}>R</div>
+                <div onClick={() => setText('S')}>S</div>
+                <div onClick={() => setText('T')}>T</div>
+                <div onClick={() => setText('U')}>U</div>
+                <div onClick={() => setText('V')}>V</div>
+                <div onClick={() => setText('W')}>W</div>
+                <div onClick={() => setText('X')}>X</div>
+                <div onClick={() => setText('Y')}>Y</div>
+                <div onClick={() => setText('Z')}>Z</div>
               </div>
             </div>
           </div>
-          <div className='single-location-search pt-50'>
-            <div className='row'>
-              <div className='col-md-6'>
-                <div className='location-header'>
-                  <div className='location-search-flex'>
+          <div className="single-location-search pt-50">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="location-header">
+                  <div className="location-search-flex">
                     <div>
                       <h3>Andhra Pradesh</h3>
                     </div>
                     <div>
                       <svg
-                        xmlns='http://www.w3.org/2000/svg'
+                        xmlns="http://www.w3.org/2000/svg"
                         width={24}
                         height={24}
-                        viewBox='0 0 24 24'
-                        fill='none'
-                        stroke='currentColor'
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
                         strokeWidth={2}
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                        className='feather feather-minus-circle'
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="feather feather-minus-circle"
                       >
                         <circle cx={12} cy={12} r={10} />
                         <line x1={8} y1={12} x2={16} y2={12} />
                       </svg>
                     </div>
                   </div>
-                  <div className='open-accordian'>
+                  <div className="open-accordian">
                     <h4>
                       Andhra Pradesh - Jangareddigudem - Near Triveni College
                     </h4>
@@ -72,55 +72,55 @@ function AlphabeticalSearch({ setText, text, centers }) {
                       Zixdo.Com, Near Triveni College, Jangareddigudem, Andhra
                       Pradesh - 534447
                     </p>
-                    <div className='call-now-button'>
-                      <div className='icon-with-button'>
+                    <div className="call-now-button">
+                      <div className="icon-with-button">
                         <div>
                           <span>
-                            <i className='fa fa-phone' />
+                            <i className="fa fa-phone" />
                           </span>
                         </div>
                         <div>Call Now Button</div>
                       </div>
-                      <div className='icon-with-button'>
+                      <div className="icon-with-button">
                         <div>
                           <span>
-                            <i className='fa fa-whatsapp' />
+                            <i className="fa fa-whatsapp" />
                           </span>
                         </div>
                         <div>Whatsapp US</div>
                       </div>
                     </div>
-                    <a href='#' className='partner'>
+                    <a href="#" className="partner">
                       Partner Details
                     </a>
                   </div>
                 </div>
               </div>
-              <div className='col-md-6'>
-                <div className='location-header'>
-                  <div className='location-search-flex'>
+              <div className="col-md-6">
+                <div className="location-header">
+                  <div className="location-search-flex">
                     <div>
                       <h3>Andhra Pradesh</h3>
                     </div>
                     <div>
                       <svg
-                        xmlns='http://www.w3.org/2000/svg'
+                        xmlns="http://www.w3.org/2000/svg"
                         width={24}
                         height={24}
-                        viewBox='0 0 24 24'
-                        fill='none'
-                        stroke='currentColor'
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
                         strokeWidth={2}
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                        className='feather feather-minus-circle'
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="feather feather-minus-circle"
                       >
                         <circle cx={12} cy={12} r={10} />
                         <line x1={8} y1={12} x2={16} y2={12} />
                       </svg>
                     </div>
                   </div>
-                  <div className='open-accordian'>
+                  <div className="open-accordian">
                     <h4>
                       Andhra Pradesh - Jangareddigudem - Near Triveni College
                     </h4>
@@ -128,25 +128,25 @@ function AlphabeticalSearch({ setText, text, centers }) {
                       Zixdo.Com, Near Triveni College, Jangareddigudem, Andhra
                       Pradesh - 534447
                     </p>
-                    <div className='call-now-button'>
-                      <div className='icon-with-button'>
+                    <div className="call-now-button">
+                      <div className="icon-with-button">
                         <div>
                           <span>
-                            <i className='fa fa-phone' />
+                            <i className="fa fa-phone" />
                           </span>
                         </div>
                         <div>Call Now Button</div>
                       </div>
-                      <div className='icon-with-button'>
+                      <div className="icon-with-button">
                         <div>
                           <span>
-                            <i className='fa fa-whatsapp' />
+                            <i className="fa fa-whatsapp" />
                           </span>
                         </div>
                         <div>Whatsapp US</div>
                       </div>
                     </div>
-                    <a href='#' className='partner'>
+                    <a href="#" className="partner">
                       Partner Details
                     </a>
                   </div>
