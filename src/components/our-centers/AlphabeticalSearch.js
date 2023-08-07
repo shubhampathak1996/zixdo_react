a00f626fc0742dc647d0344f4a1fde63766a6093
shubham pathak <shubhampathak1996@gmail.com>
2023-08-07 17:08:16 +0530

    update

--- src/components/our-centers/AlphabeticalSearch.js
+++ src/components/our-centers/AlphabeticalSearch.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function AlphabeticalSearch({ setText, text, centers }) {
   console.log('centers', centers);
   return (
     <>
-      <section className='sec-50 alphabetical-search'>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-md-12'>
-              <div className='alpha-box'>
+      <section className="sec-50 alphabetical-search">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="alpha-box">
                 <div onClick={() => setText('A')}>A</div>
                 <div onClick={() => setText('B')}>B</div>
                 <div onClick={() => setText('C')}>C</div>
@@ -38,43 +39,43 @@ function AlphabeticalSearch({ setText, text, centers }) {
               </div>
             </div>
           </div>
-          <div className='single-location-search pt-50'>
-            <div className='row'>
+          <div className="single-location-search pt-50">
+            <div className="row">
               {centers &&
                 centers.map((item) => {
                   return (
-                    <div className='col-md-6'>
-                      <div className='location-header'>
-                        <div className='location-search-flex'>
+                    <div className="col-md-6">
+                      <div className="location-header">
+                        <div className="location-search-flex">
                           <div>
                             <h3> {item.store_name} </h3>
                           </div>
                           <div>
                             <svg
-                              xmlns='http://www.w3.org/2000/svg'
+                              xmlns="http://www.w3.org/2000/svg"
                               width={24}
                               height={24}
-                              viewBox='0 0 24 24'
-                              fill='none'
-                              stroke='currentColor'
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              stroke="currentColor"
                               strokeWidth={2}
-                              strokeLinecap='round'
-                              strokeLinejoin='round'
-                              className='feather feather-minus-circle'
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              className="feather feather-minus-circle"
                             >
                               <circle cx={12} cy={12} r={10} />
                               <line x1={8} y1={12} x2={16} y2={12} />
                             </svg>
                           </div>
                         </div>
-                        <div className='open-accordian'>
+                        <div className="open-accordian">
                           <p>{item.store_complete_address}</p>
-                          <div className='call-now-button'>
+                          <div className="call-now-button">
                             <a href={`tel:${item.store_contact_number}`}>
-                              <div className='icon-with-button'>
+                              <div className="icon-with-button">
                                 <div>
                                   <span>
-                                    <i className='fa fa-phone' />
+                                    <i className="fa fa-phone" />
                                   </span>
                                 </div>
                                 <div>Call Now </div>
@@ -83,19 +84,19 @@ function AlphabeticalSearch({ setText, text, centers }) {
                             <a
                               href={`https://api.whatsapp.com/send?phone=91${item.store_contact_number}&text=Hello%20I%20am%20interested%20in%20car%20wash`}
                             >
-                              <div className='icon-with-button'>
+                              <div className="icon-with-button">
                                 <div>
                                   <span>
-                                    <i className='fa fa-whatsapp' />
+                                    <i className="fa fa-whatsapp" />
                                   </span>
                                 </div>
                                 <div>Whatsapp </div>
                               </div>
                             </a>
                           </div>
-                          <a href='#' className='partner'>
+                          <Link to="/store/:order_id" className="partner">
                             Partner Details
-                          </a>
+                          </Link>
                         </div>
                       </div>
                     </div>
